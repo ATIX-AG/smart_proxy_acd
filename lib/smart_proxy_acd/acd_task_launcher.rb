fbb2b7cc856f09141be50661fdcaa267b26c7ea1
Adam Ruzicka <aruzicka@redhat.com>
2021-05-06 15:16:30 +0200

    Make rubocop happy

--- lib/smart_proxy_acd/acd_task_launcher.rb
+++ lib/smart_proxy_acd/acd_task_launcher.rb
@@ -1,23 +1,25 @@
-module Proxy::Acd
-  # Implements the TaskLauncher::Batch for Acd
-  class AcdTaskLauncher < ForemanTasksCore::TaskLauncher::Batch
-    # Implements the Runner::Action for Acd
-    class AcdRunnerAction < ForemanTasksCore::Runner::Action
-      def initiate_runner
-        additional_options = {
-          :step_id => run_step_id,
-          :uuid => execution_plan_id
-        }
-        ::Proxy::Acd::AcdRunner.new(
-          input.merge(additional_options),
-          :suspended_action => suspended_action
-        )
+module Proxy
+  module Acd
+    # Implements the TaskLauncher::Batch for Acd
+    class AcdTaskLauncher < ForemanTasksCore::TaskLauncher::Batch
+      # Implements the Runner::Action for Acd
+      class AcdRunnerAction < ForemanTasksCore::Runner::Action
+        def initiate_runner
+          additional_options = {
+            :step_id => run_step_id,
+            :uuid => execution_plan_id
+          }
+          ::Proxy::Acd::AcdRunner.new(
+            input.merge(additional_options),
+            :suspended_action => suspended_action
+          )
+        end
       end
-    end
 
-    def child_launcher(parent)
-      ForemanTasksCore::TaskLauncher::Single.new(world, callback, :parent => parent,
-                                                                  :action_class_override => AcdRunnerAction)
+      def child_launcher(parent)
+        ForemanTasksCore::TaskLauncher::Single.new(world, callback, :parent => parent,
+                                                                    :action_class_override => AcdRunnerAction)
+      end
     end
   end
 end
